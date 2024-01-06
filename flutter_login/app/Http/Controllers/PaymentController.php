1b2c7eafee5baa9c2fccf96454de9ede09243ca1
Rajavignesh <rvthara96@gmail.com>
2024-01-06 08:07:39 +0530

    added acl

--- flutter_login/app/Http/Controllers/PaymentController.php
+++ flutter_login/app/Http/Controllers/PaymentController.php
@@ -17,26 +17,26 @@ class PaymentController extends Controller
             'due_date'=>'required|date',
         ]);
 
+        $user= User::find($request->user_id);
+        if(!$user)
+        {
+         return response()->json(['error'=>'User Not Found'],404);
+        }
+
         $existuserid=Payment::
-        //where('user_id',$request->user_id)
-        where('due_date', $request->due_date)
+        where('user_id',$request->user_id)->
+        where('payment_status','pending')
         ->first();
         if($existuserid)
         {
             return response()->json(['Error'=>'This user Due date already present in Payment table for this month'],422);
         }
 
-       $user= User::find($request->user_id);
-       if(!$user)
-       {
-        return response()->json(['error'=>'User Not Found'],404);
-       }
-
        $payment=Payment::create([
         'user_id'=>$request->user_id,
         'payment_amount'=>$request->payment_amount,
         'due_date'=>$request->due_date,
-       ]);
+       ])->where($request->user_id);
 
        return response()->json(['Payment Due date alert send successfully'=>true,'payment'=>$payment]);
 
